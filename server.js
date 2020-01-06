f9715c13cde6d70fa571ffeff72f3e3e8d50a98b
Nicolas <larochenicolas.info@gmail.com>
2020-01-06 09:53:20 +0100

    correction fichier server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const { Client } = require('pg');
+const redis = require('redis')
 
 // Constants
 const PORT = 8080;
@@ -22,13 +23,26 @@ app.get('/', (req, res) => {
 app.get('/api', (req, res) => {
   res.send( { "message": "Hello World" });
 });
-app.get('/status', (req, res) => {
-  res.send({
-    "status": "OK",
-    "postgresUptime": "String",
-    "redisConnectedClients": "Number"
-  });
-});
+app.get('/status', async (req, res) => {
+  const postgresQuery = 'SELECT date_trunc(\'second\', current_timestamp - pg_postmaster_start_time()) as uptime;'
+  const result = await pg.query(postgresQuery);
+  const uptime = result.rows[0].uptime;
+  const uptimeString = () => {
+    let time = '';
 
+    time += uptime.hours ? `${uptime.hours}h ` : '';
+    time += uptime.minutes ? `${uptime.minutes}m ` : '';
+    time += uptime.seconds ? `${uptime.seconds}s` : '';
+
+    return time
+  };
+
+  res.json({
+    status: 'ok',
+    postgresUptime: uptimeString(),
+    redisConnectedClients: Number(redisClient.server_info.connected_clients)
+  })
+});
+pg.connect();
 app.listen(PORT);
 console.log('le serveur est lancer au port'+ PORT);
